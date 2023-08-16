408ac3b6fb4f0d674f4a16b223ec184024eea3bd
SuruthiFruma <118813322+SuruthiFruma@users.noreply.github.com>
2023-08-16 11:21:35 +0530

    Add files via upload
    Episode 5:Lets get Hooked

---
+++ Episode 5:Lets get Hooked/Coding/src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+      <div className="header">
+        <h2 className="title">Food Clone App</h2>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  export default Header
